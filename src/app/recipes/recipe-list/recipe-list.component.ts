0eb0d490f6d0c51309bfa4f60ba4c44059990531
MadaZZ <sparshranaazz@gmail.com>
2019-04-15 09:38:01 +0530

    Added dummy recipe list
    Added dummy list to recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe 1',
+    'This is a test recipe1',
+    'https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg'),
+    new Recipe('Test Recipe 2',
+    'This is a test recipe2',
+    'https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg'),
+  ];
   constructor() { }
 
   ngOnInit() {
