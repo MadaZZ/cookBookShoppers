09b62a36826559a8f1a490e065b2f04bd09a2433
MadaZZ <sparshranaazz@gmail.com>
2019-05-08 16:36:43 +0530

    Added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cookBookShoppers';
+  loadedType = 'recipe';
+  
+  onNavigate(type: string){
+    this.loadedType = type;
+  }
+
 }
